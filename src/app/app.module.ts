92b7e034c04878f3046e3a967865cd6b70632819
Oscar <oscar.espinozaea19@gmail.com>
2022-03-14 17:44:48 -0600

    EndCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule}from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { GiftModule } from './gift/gift.module';
+import { ShareModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ShareModule,
+    GiftModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
